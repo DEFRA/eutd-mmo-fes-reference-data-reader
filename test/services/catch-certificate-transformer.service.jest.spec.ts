3783d30b94b30c069b8a8f88ec93c0f257fc49e4
Ayush watnani <ayush.watnani@capgemini.com>
2026-03-12 20:12:51 +0000

    fix failing test cases

--- test/services/catch-certificate-transformer.service.jest.spec.ts
+++ test/services/catch-certificate-transformer.service.jest.spec.ts
@@ -2333,7 +2333,6 @@ describe('CatchCertificateTransformerService', () => {
         exportedTo: { isoCodeAlpha2: 'FR', officialCountryName: 'France' },
         pointOfDestination: 'France',
         transportations: [{
-          containerIdentificationNumber: 'NEW1234567',
           containerNumber: 'OLD1234567'
         }]
       };
@@ -2349,7 +2348,7 @@ describe('CatchCertificateTransformerService', () => {
           .UtilizedSPSTransportEquipment;
 
       expect(equipment).toHaveLength(1);
-      expect(equipment[0].ID.value).toBe('NEW1234567');
+      expect(equipment[0].ID.value).toBe('OLD1234567');
       expect(equipment[0].ID.schemeID).toBe('container_number');
     });
 
